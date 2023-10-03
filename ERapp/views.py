74a887a51c11ce0baaa5405625b0b8cc1a7dbb1d
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-10-04 00:09:46 +0100

    v0.0.14.6 stable

--- ERapp/views.py
+++ ERapp/views.py
@@ -167,43 +167,37 @@ def generate_random_string(length):
 def table_view(request): # add row
 
     if request.method == 'POST' :
-        #if request.FILES['cell_data5add']:
-        cell_id = generate_random_string(10)
+        myID=request.POST.get("myid")
         
-        firstname = request.POST.get("cell_data1add")
-        lastname = request.POST.get("cell_data2add")
-        address = request.POST.get("cell_data3add")
-        num = request.POST.get("cell_data4add")
-        vt1 = request.FILES.getlist('cell_data5add')
-        etat = request.POST.get("cell_data6add")
-        #tp = request.POST.get("cell_data7")
-        #auditV1 = request.FILES["cell_data8"]
-        #auditV2 = request.FILES["cell_data9"]
-        #auditV3 = request.FILES["cell_data10"]
-        #coffrac = request.POST.get("cell_data11")
-        
-        up_files = ""; 
-        #for uploaded_file in vt:
-            #up_files+=uploaded_file + " "
-        #form = TableDataForm1(request.POST)
-        #if firstname or lastname or address or num and etat:
-        #data = TableData001(cell_id=cell_id ,firstname=firstname ,lastname=lastname ,address=address ,num=num ,vt=vt ,etat=etat ,tp=tp ,auditV1=auditV1 ,auditV2=auditV2 ,auditV3=auditV3 ,coffrac=coffrac)
-        for uploaded_file in vt1:
-            TableData001.objects.create(
-                cell_id=cell_id, 
-                firstname=firstname, 
-                lastname=lastname, 
-                address=address, 
-                num=num, 
-                vt=uploaded_file, 
-                etat=etat  
-            )
+        l="table1_input_files_to_"+str(myID)
+        if request.POST.get("mybutton") == 'clicked':
+            inp_files=request.FILES.getlist(l)
+            #inp_files=request.FILES["table1_input_files_to_"+str(myID)]
+
+            for file in request.FILES.getlist(l):
+                if file.name.split(".")[1] in ['jpg','png','jpeg','heic']:
+                    format_file="image"
+                if file.name.split(".")[1] in ['doc','docx']:
+                    format_file="excel"
+                    
+                if file.name.split(".")[1] in ['xls','xlsm']:
+                    format_file="excel"  
+                file_table_auditV1.objects.create(
+                    file_id = myID,
+                    file_name = file.name,
+                    file_save = file,
+                    file_format =format_file
+                
+                )
+                
             
-        #data = TableData001(cell_id=cell_id ,firstname=firstname ,lastname=lastname ,address=address ,num=num , vt=up_files  ,etat=etat )
+            
+   
+            
         
-        #data = TableData001(cell_data=cell_data1,cell_data2=cell_data2)
         
-        #data.save()
+        
+        
         return redirect('/formT/')
     
     
