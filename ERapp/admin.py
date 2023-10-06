461402ef6f9fbff36bf5bab09638bcae4eea6293
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-10-06 13:41:23 +0100

    v0.0.15.8.3 {cleaning some functions}

--- ERapp/admin.py
+++ ERapp/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from .models import TableData1,USER,ImageModel,TableData,TableData01,TableData001,file_table_auditV1,file_table_auditV2,file_table_auditV3,file_table_auditFinal,file_table_vt
+from .models import USER,ImageModel,TableData001,file_table_auditV1,file_table_auditV2,file_table_auditV3,file_table_auditFinal,file_table_vt
 # Register your models here.
 
 admin.site.register(TableData001)
-admin.site.register(TableData1)
+
 admin.site.register(USER)
 admin.site.register(ImageModel)
-admin.site.register(TableData01)
-admin.site.register(TableData)
+
 admin.site.register(file_table_auditV1)
 admin.site.register(file_table_auditV2)
 admin.site.register(file_table_auditV3)
--- ERapp/models.py
+++ ERapp/models.py
@@ -19,44 +19,16 @@ class USER(models.Model):
         return f'{self.username}'
 
     
-class TableData1(models.Model):
-    cell_data = models.CharField(max_length=255)
-    cell_data2 = models.CharField(max_length=255)
-    def __str__(self):
-        return f'{self.cell_data}'
-    
+
 
 class ImageModel(models.Model):
     user_id = models.CharField(max_length=255)
     imageTitle =models.CharField(max_length=255)
     image = models.ImageField(upload_to='uploads/profilePic')
     
-class TableData(models.Model):
-    cell_data = models.CharField(max_length=255)
-    cell_data2 = models.CharField(max_length=255)
-    def __str__(self):
-        return f'{self.cell_data}'
 
 
 
-class TableData01(models.Model):
-    cell_data = models.CharField(max_length=255, default=None, blank=True, null=True)
-    cell_data2 = models.CharField(max_length=255, default=None, blank=True, null=True)
-    cell_data3 = models.CharField(max_length=255, default=None, blank=True, null=True)
-    cell_data4 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data5 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data6 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data7 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data8 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data9 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data10 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data11 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    cell_data12 = models.CharField(max_length=500, default=None, blank=True, null=True)
-    
-
-    
-    def __str__(self):
-        return f'{self.cell_data}'
     
 class TableData001(models.Model):
     cell_id = models.CharField(max_length=255,blank=True)
