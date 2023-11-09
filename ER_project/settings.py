f7517f9e345463f620d94d14f4851def4fb34feb
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-11-09 18:29:48 +0100

    Deploying to Railway 9

--- ER_project/settings.py
+++ ER_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xe_ocjnzc+-l$)ni!-e0u!bh$!nt5xco#c55+0e+7*mzh=ded0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # this sould be False
+DEBUG = False # this sould be False
 
 #CSRF_TRUSTED_ORIGINS =["web-production-68e2.up.railway.app",
 #                       "https://web-production-68e2.up.railway.app"]
