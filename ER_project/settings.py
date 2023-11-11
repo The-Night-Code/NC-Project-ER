ec9e2790cd2f95fde4a40ba9e48c1dfd3e9cf76d
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-11-11 18:37:52 +0100

    deploy to railway 5.5

--- ER_project/settings.py
+++ ER_project/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -128,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static',"ERapp/static",
+STATICFILES_DIRS = [BASE_DIR / 'static',#"ERapp/static",
                     BASE_DIR / "media"]
 
 MEDIA_ROOT = BASE_DIR / "media"
---
+++ staticfiles/js/main6.js
@@ -0,0 +1,42 @@
+function upload_image_formK_click_button(inp){
+
+    // Set the values of the hidden input fields in the form
+    document.getElementById("input_value").value = inp;
+
+    // Check if the input file is not empty
+    const fileInput = document.getElementsByName(inp)[0];
+    if (fileInput && fileInput.files.length > 0) {
+        // If not empty, click the "mybutton1"
+        document.getElementById("mybutton1").click();
+    } 
+
+  }
+
+
+function Geolocalisation(){
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            var altitude = position.coords.altitude;
+            
+            document.getElementById("latitude").textContent = latitude;
+            document.getElementById("longitude").textContent = longitude;
+            document.getElementById("latitude_input").value = latitude;
+            document.getElementById("longitude_input").value = longitude;
+            document.getElementById("Geolocalisation_div").style.display = "block";
+            if (altitude !== null) {
+                document.getElementById("altitude").textContent = altitude + " meters";
+                document.getElementById("altitude_input").value = altitude + " meters";
+            } else {
+                document.getElementById("altitude").textContent = "";
+            }
+            
+        });
+    } else {
+        alert("Geolocation is not available in your browser.");
+    }
+}
+
+
+    
