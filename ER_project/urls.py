192e7e9d115d9956c4b109c28fee025ffaf94432
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-11-11 17:51:40 +0100

    deploy to railway 1

--- ER_project/urls.py
+++ ER_project/urls.py
@@ -90,7 +90,7 @@ urlpatterns = [
         
     path('save_signature/', save_signature, name='save_signature'),
     
-] #+static(settings.MEDIA_URL, documnet_root=settings.MEDIA_ROOT)
+] # + static(settings.MEDIA_URL, documnet_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
