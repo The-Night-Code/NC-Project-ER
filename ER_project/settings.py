167bb45c7e554314f516272bffd0cf9e2f2805f9
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-11-11 18:02:13 +0100

    deploy to railway 4

--- ER_project/settings.py
+++ ER_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xe_ocjnzc+-l$)ni!-e0u!bh$!nt5xco#c55+0e+7*mzh=ded0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'FALSE' # this should be False  'FALSE'
+DEBUG = True # this should be False  'FALSE'
 
 ALLOWED_HOSTS = ['web-production-3f42.up.railway.app']
 CSRF_TRUSTED_ORIGINS=["https://web-production-3f42.up.railway.app/"]
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     
 
 ]
