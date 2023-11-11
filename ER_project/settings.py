3efc3749ec54db678b21143875d323782996edf7
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-11-11 18:15:20 +0100

    deploy to railway 5.2

--- ER_project/settings.py
+++ ER_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static',"ERapp/static"
+STATICFILES_DIRS = [BASE_DIR / 'static',"ERapp/static",
                     BASE_DIR / "media"]
 
 MEDIA_ROOT = BASE_DIR / "media"
