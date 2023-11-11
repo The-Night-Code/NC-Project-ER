6d20c3503977910f11e9342c4a23fdae2ca4c769
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-11-11 19:12:19 +0100

    v0.0.27.8

--- ER_project/settings.py
+++ ER_project/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static',#"ERapp/static",
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'ERapp/static',#"ERapp/static",
                     BASE_DIR / "media"]
 
 MEDIA_ROOT = BASE_DIR / "media"
