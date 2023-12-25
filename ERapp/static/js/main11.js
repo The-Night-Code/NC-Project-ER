bc01c349569045e6797eb16254b78c8b7c58fa74
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-12-25 02:35:32 +0100

    v0.0.37.6 {re-constructing chat box 'stable'}

--- ERapp/static/js/main11.js
+++ ERapp/static/js/main11.js
@@ -18,10 +18,6 @@ chatSocket.onmessage = function (e) {
     div.innerHTML = data.email + " : " + data.message;
     document.querySelector("#input_msg_"+data.col+"_"+data.cellId).value = "";
     var newMessage = `
-
-        
-        
-        
         <div class="message-item row_chat_popup chat_popup_content_si">
 
             <div class="chat_popup_content_si_header">
@@ -39,52 +35,22 @@ chatSocket.onmessage = function (e) {
             </div>
             <p class="chat_popup_content_si_timer">quelques secondes</p>
         </div>
-        
-        
-        
-        
-        
         `;
-
-    // Append the new message to the chat box
     $("#chat-messages-container_"+data.col+"_"+data.cellId).prepend(newMessage);
-    
-// The event listener will log: null
-
 };
 
 function OPEN_popUP_chat(id){
-
-
-    
-    
-      
-    
-    const div_popUP_chat = document.querySelector('.chat_popup');
-
     var divsToHide = document.getElementsByClassName("chat_popup"); //divsToHide is an array
     for(var i = 0; i < divsToHide.length; i++){
-        //divsToHide[i].style.visibility = "hidden"; // or
-        divsToHide[i].style.display = "none"; // depending on what you're doing
+        divsToHide[i].style.display = "none"; 
     }
-
     document.getElementById(id).style.display ="block";
-    //document.getElementById(id).style.display = ;
-
-
-
-  }
+}
   
-
-  
+ 
 function CLOSE_popUP_chat(id) {
-    //document.querySelector('.chat_popup').style.display ="none";
-
     var divsToHide = document.getElementsByClassName("chat_popup"); //divsToHide is an array
     for(var i = 0; i < divsToHide.length; i++){
-        //divsToHide[i].style.visibility = "hidden"; // or
-        divsToHide[i].style.display = "none"; // depending on what you're doing
+        divsToHide[i].style.display = "none"; 
     }
-//document.getElementById(id).classList.remove("chat_popup_show");
-//document.getElementById(id).classList.add("chat_popup_hide");
 }
